458fad78d86c38ef0566ec07eba2ca73da49c270
LRZDX <zyq759316417@outlook.com>
2021-03-31 11:39:36 +0800

    + init

---
+++ test_package/test_package.c
@@ -0,0 +1,11 @@
+#include <flann/flann.h>
+
+#include <stdio.h>
+
+int main()
+{
+    // Simply make sure that it builds an runs
+    flann_log_verbosity(1);
+    printf("test complete\n");
+    return 0;
+}
